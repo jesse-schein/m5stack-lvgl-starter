3203a5f78c54b05f79d98f504e21d8b0b4f3e883
Staars <Baars@gmx.de>
2020-07-27 22:21:24 +0200

    add examples

--- src/main.c
+++ src/main.c
@@ -8,10 +8,13 @@
   ******************************************************************************
 */
 
+
 #include "lvgl.h"
+#include "../lv_ex_conf.h"
+#include "../lv_examples.h"
 #include "app_hal.h"
 
-#include "demo.h"
+// #include "lv_demo_benchmark.h"
 
 int main(void)
 {
@@ -20,6 +23,13 @@ int main(void)
 	hal_setup();
 
 	lv_demo_benchmark();
+  // lv_ex_get_started1();
+  // lv_ex_get_started2();
+  // lv_ex_get_started3();
+  // lv_demo_widgets();
+  // lv_demo_printer();
+  // lv_demo_keypad_encoder();
+  // lv_demo_stress();
 
 	hal_loop();
 }
